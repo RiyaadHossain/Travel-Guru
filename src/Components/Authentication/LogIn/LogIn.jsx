07db73cbc75191ea43bc085b1a59ffc89698cc60
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-16 18:20:42 +0600

    Sign in works

--- src/Components/Authentication/LogIn/LogIn.jsx
+++ src/Components/Authentication/LogIn/LogIn.jsx
@@ -1,21 +1,70 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { async } from "@firebase/util";
+import React, { useEffect, useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
 import Facebook from "../../../Assets/Icons/fb.png";
 import Google from "../../../Assets/Icons/google.png";
+import auth from "../../../Firebase/Firebase.init";
 
 const LogIn = () => {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  
+    useEffect(() => {
+      if (user) {
+        toast.success("Signed In", { id: "test" });
+        navigate("/");
+      }
+    }, [user, navigate]);
+  
+  // Function: Sign in
+  const onFormSubmit = async(e) => {
+    e.preventDefault()
+    await signInWithEmailAndPassword(email, password)
+
+    // Not working the following code :(
+    if (user) {
+      navigate('/')
+      toast.success("Signed In")
+    }
+  }
   return (
     <div>
       <div className="border rounded p-6 form-container">
         <h3 className="text-3xl font-semibold mt-3 mb-5 text-white">Login</h3>
-        <form>
-          <input className="block outline-none w-full bg-transparent border-b mb-4 py-3 pl-4 text-lg" type="email" name="email" placeholder="Email" />
-          <input className="block outline-none w-full bg-transparent border-b mb-4 py-3 pl-4 text-lg" type="password" name="password" placeholder="Password" />
-                  <input type="checkbox" className="mt-3" id="scales"/>
-                  <label for="scales" className="text-white ml-2 ">Remember Me</label>
-          <input type="submit" value="Login"  className="block bg-yellow-500 w-full py-3 rounded text-xl mt-16"/>
+        <form onSubmit={onFormSubmit}>
+          <input
+            onBlur={(e) => setEmail(e.target.value)}
+            className="block outline-none w-full bg-transparent border-b mb-4 py-3 pl-4 text-lg"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <input
+            onBlur={(e) => setPassword(e.target.value)}
+            className="block outline-none w-full bg-transparent border-b mb-4 py-3 pl-4 text-lg"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <input type="checkbox" className="mt-3" id="scales" />
+          <label for="scales" className="text-white ml-2 ">
+            Remember Me
+          </label>
+          <input
+            type="submit"
+            value="Login"
+            className="block bg-yellow-500 w-full py-3 rounded text-xl mt-16"
+          />
           <p className="text-white text-center mt-5 text-lg ">
-            Don't have an account? <Link className="text-yellow-500" to={"/signup"}>Create an account</Link>
+            Don't have an account?{" "}
+            <Link className="text-yellow-500" to={"/signup"}>
+              Create an account
+            </Link>
           </p>
         </form>
       </div>
@@ -29,7 +78,9 @@ const LogIn = () => {
       <div className=" w-full">
         <button className="flex items-center border w-1/3 px-[6px] mb-3 py-1 rounded-full mx-auto">
           <img className="w-10" src={Facebook} alt="" />
-          <span className="text-lg ml-4 text-white">Continue with Facebook</span>
+          <span className="text-lg ml-4 text-white">
+            Continue with Facebook
+          </span>
         </button>
         <button className="flex items-center border w-1/3 px-[6px] py-1 rounded-full mx-auto">
           <img className="w-10" src={Google} alt="" />
